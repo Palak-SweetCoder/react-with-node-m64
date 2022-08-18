5a12952ad784bead96d1435d7984222aa072f161
Palak-SweetCoder <pspalak6@gmail.com>
2022-08-19 01:53:16 +0600

    react node post get

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = {name, email};
+    // console.log(name, email);
+    // post data to server 
+    fetch('http://localhost:5000/user', {
+      method: 'POST',
+      headers:{
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      const newUsers = [...users, data];
+      setUsers(newUsers);
+      console.log(data);
+    })
+  }
+
+  return (
+    <div>
+      <h1>My Own Data: {users.length}</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="name" placeholder='Name' required/>
+        <input type="text" name="email" id="email" placeholder='Email' required/>
+        <input type="submit" value="Add User"/>
+      </form>
+      <ul>
+        {
+          users.map(user => <li key={user.id}>ID: {user.id} Name: {user.name} Email: {user.email}</li>)
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
